4992179c8b384688b54c356ce3d860277b6773ca
djaghah <35106553+djaghah@users.noreply.github.com>
2024-05-21 10:54:23 +0300

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const footerContent = `
         <footer>
             <p>If you like our project, consider donating any token or NFT/SFT for further development:</p>
-            <p><strong>Multiversx address:</strong> erd1lse0wg9mr7pvxgjh7enfu6gycz8w4hm7uy4hwx4ukt97sc6yl55slxlylf</p>
+            <p><strong>Multiversx address:</strong> <span style="color: #ffcc00;">erd1lse0wg9mr7pvxgjh7enfu6gycz8w4hm7uy4hwx4ukt97sc6yl55slxlylf</span></p>
+            <p>Thank you for your support!</p>
         </footer>
     `;
     document.getElementById("footer-placeholder").innerHTML = footerContent;
